a70f4492f6e449a39408e089b010a342209ae971
Kevin Morgan <kmorgan26@gmail.com>
2022-04-21 15:47:22 -0500

    Initial Program.cs Configuration for Client

--- DRApplication/Client/Program.cs
+++ DRApplication/Client/Program.cs
@@ -1,11 +1,17 @@
+using Blazored.LocalStorage;
 using DRApplication.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddMudServices();
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
