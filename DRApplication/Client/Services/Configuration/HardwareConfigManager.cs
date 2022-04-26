b213498bb8a2ffdf9db89855db5f70d487ee7baf
Kevin.D.Morgan <Kevin.D.Morgan@Gdit.com>
2022-04-26 08:43:04 -0500

    Create HardwareConfig Manager

---
+++ DRApplication/Client/Services/Configuration/HardwareConfigManager.cs
@@ -0,0 +1,14 @@
+﻿using DRApplication.Shared.Models.ConfigurationModels;
+
+namespace DRApplication.Client.Services.Configuration
+{
+    public class HardwareConfigManager : ApiRepository<HardwareConfig>
+    {
+        HttpClient http;
+
+        public HardwareConfigManager(HttpClient _http) : base(_http, "hardwareconfig", "Id")
+        {
+            http = _http;
+        }
+    }
+}
