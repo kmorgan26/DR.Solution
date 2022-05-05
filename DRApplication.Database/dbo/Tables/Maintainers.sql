9be33d39d39bd9cdc4c24f4ff9e7d142110c862e
Kevin Morgan <kmorgan26@gmail.com>
2022-05-05 09:22:39 -0500

    Update MaintainerName Property

--- DRApplication.Database/dbo/Tables/Maintainers.sql
+++ DRApplication.Database/dbo/Tables/Maintainers.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Maintainers] (
     [Id]   INT            IDENTITY (1, 1) NOT NULL,
-    [MaintainerName] NVARCHAR (255) NOT NULL,
+    [Name] NVARCHAR (255) NOT NULL,
     CONSTRAINT [PK_Maintainers] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
