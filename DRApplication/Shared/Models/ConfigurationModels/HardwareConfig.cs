7e15f55b5ada3feef5aa1bebbfa7f678209f60c6
Kevin Morgan <kmorgan26@gmail.com>
2022-05-05 13:42:29 -0500

    Remove Virtual properties from Hardware Config

--- DRApplication/Shared/Models/ConfigurationModels/HardwareConfig.cs
+++ DRApplication/Shared/Models/ConfigurationModels/HardwareConfig.cs
@@ -6,20 +6,20 @@ namespace DRApplication.Shared.Models.ConfigurationModels
     [Table("HardwareConfigs")]
     public partial class HardwareConfig
     {
-        public HardwareConfig()
-        {
-            HardwareVersionsConfigs = new HashSet<HardwareVersionsConfig>();
-            SoftwareSystems = new HashSet<SoftwareSystem>();
-        }
+        //public HardwareConfig()
+        //{
+        //    HardwareVersionsConfigs = new HashSet<HardwareVersionsConfig>();
+        //    SoftwareSystems = new HashSet<SoftwareSystem>();
+        //}
 
         [Key]
         public int Id { get; set; }
         public string Name { get; set; } = null!;
         public int DeviceTypeId { get; set; }
 
-        public virtual DeviceType DeviceType { get; set; } = null!;
-        public virtual ICollection<HardwareVersionsConfig> HardwareVersionsConfigs { get; set; }
-        public virtual ICollection<SoftwareSystem> SoftwareSystems { get; set; }
+        //public virtual DeviceType DeviceType { get; set; } = null!;
+        //public virtual ICollection<HardwareVersionsConfig> HardwareVersionsConfigs { get; set; }
+        //public virtual ICollection<SoftwareSystem> SoftwareSystems { get; set; }
     }
 }
 
