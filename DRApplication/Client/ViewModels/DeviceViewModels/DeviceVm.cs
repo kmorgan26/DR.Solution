ba22951ea1d457efbe5c4c2f1694cf9ea05b1552
Kevin Morgan <kmorgan26@gmail.com>
2022-05-09 07:19:47 -0500

    Create and implement DeviceTypeLabel

--- DRApplication/Client/ViewModels/DeviceViewModels/DeviceVm.cs
+++ DRApplication/Client/ViewModels/DeviceViewModels/DeviceVm.cs
@@ -8,8 +8,7 @@ namespace DRApplication.Client.ViewModels.DeviceViewModels
         
         public string Name { get; set; } = string.Empty;
         
-        [Display(Name = "Device Type")]
-        public string DeviceTypeName { get; set; } = string.Empty;
+        public int DeviceTypeId { get; set; }
         
         public bool IsActive { get; set; }
 
