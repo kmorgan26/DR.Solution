16a282c7fb10be3cec1c98a443908d25c7dca53d
Kevin Morgan <kmorgan26@gmail.com>
2022-05-10 06:25:00 -0500

    moved Device and DT to code-behind

---
+++ DRApplication/Client/Pages/Platforms/DeviceTypes/DeviceTypeEdit.razor.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Components;
+
+namespace DRApplication.Client.Pages.Platforms
+{
+    public partial class DeviceTypeEdit
+    {
+        [Parameter]
+        public int DeviceTypeId { get; set; }
+    }
+}
---
+++ DRApplication/Client/Pages/Platforms/Devices/DeviceEdit.razor.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Components;
+
+namespace DRApplication.Client.Pages.Platforms
+{
+    public partial class DeviceEdit
+    {
+        [Parameter]
+        public int DeviceId { get; set; }
+    }
+}
