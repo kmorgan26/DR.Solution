dad20ebb4e02ee57c847b05f5e9a7c7fc2584739
Kevin Morgan <kmorgan26@gmail.com>
2022-05-10 06:20:19 -0500

    Moved code-behind

---
+++ DRApplication/Client/Pages/Platforms/Maintainers/MaintainerEdit.razor.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Components;
+
+namespace DRApplication.Client.Pages.Platforms
+{
+    public partial class MaintainerEdit
+    {
+        [Parameter]
+        public int MaintainerId { get; set; }
+    }
+}
