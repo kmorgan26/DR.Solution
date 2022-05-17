0c9993712d755e72d55f84569158082305e238e3
Kevin Morgan <kmorgan26@gmail.com>
2022-05-17 16:09:10 -0500

    Created ITemplate Interface

---
+++ DRApplication/Client/Interfaces/ITemplateService.cs
@@ -0,0 +1,11 @@
+﻿using DRApplication.Client.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DRApplication.Client.Interfaces
+{
+    public interface ITemplateService
+    {
+        Task<object> GetTableRequestByViewModelName(string viewModelName);
+
+    }
+}
---
+++ DRApplication/Client/Services/TemplateService.cs
@@ -0,0 +1,16 @@
+﻿using DRApplication.Client.Interfaces;
+using DRApplication.Client.Requests;
+using DRApplication.Client.ViewModels.Platform;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DRApplication.Client.Services
+{
+    public class TemplateService : ITemplateService
+    {
+        public Task<object> GetTableRequestByViewModelName(string viewModelName)
+        {
+            throw new NotImplementedException();
+        }
+        
+    }
+}
