b22c42384e297bc88fdeac8c9ac0a9aa7161a54c
Kevin Morgan <kmorgan26@gmail.com>
2022-05-17 16:10:25 -0500

    Create request for Table Service

---
+++ DRApplication/Client/Requests/TableRequest.cs
@@ -0,0 +1,21 @@
+﻿using MudBlazor;
+
+namespace DRApplication.Client.Requests
+{
+    public class TableRequest<TEntity> where TEntity : class
+    {
+        /// <summary>
+        /// This will the the part of the Route that is unique to the table
+        /// Example: device/edit or device/details
+        /// </summary>
+        public string? UrlSegment { get; set; }
+        
+        /// <summary>
+        /// An optional value to override the default icon assigned
+        /// </summary>
+        public string? Icon { get; set; }
+
+        public IEnumerable<TEntity>? Data { get; set; }
+
+    }
+}
