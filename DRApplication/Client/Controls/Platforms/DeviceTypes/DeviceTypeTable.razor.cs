6e11dbc1c0d3090c480e08d27705af9cf6ea861f
Kevin Morgan <kmorgan26@gmail.com>
2022-05-24 13:42:41 -0500

    Create DeviceType Table with FSMudTable

--- DRApplication/Client/Controls/Platforms/DeviceTypes/DeviceTypeTable.razor.cs
+++ DRApplication/Client/Controls/Platforms/DeviceTypes/DeviceTypeTable.razor.cs
@@ -9,18 +9,18 @@ public partial class DeviceTypeTable
     [Parameter]
     public IEnumerable<DeviceTypeVm>? DeviceTypeVms { get; set; }
 
-    private List<MaintainerVm>? _maintainers;
-
-    private async Task GetMaintainerVms()
-    {
-        var list = await PlatformService.GetMaintainerVmsAsync();
-        _maintainers = list.ToList();
-    }  
+    private List<MaintainerVm> _maintainers = new();
 
     private async Task UpdateRowItem(object e)
     {
         var viewModel = (DeviceTypeVm)e;
         var model = Mapping.Mapper.Map<DeviceType>(viewModel);
         await _db.UpdateAsync(model);
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        var list = await PlatformService.GetMaintainerVmsAsync();
+        _maintainers = list.ToList();
     }
 }
