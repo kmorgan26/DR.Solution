563f5c1d3d95e7ba7101177721650abea66a4f82
Kevin Morgan <kmorgan26@gmail.com>
2022-05-25 14:23:44 -0500

    modified namespace directive

--- DRApplication/Shared/Filters/PaginationFilter.cs
+++ DRApplication/Shared/Filters/PaginationFilter.cs
@@ -1,15 +1,14 @@
-﻿namespace DRApplication.Shared.Filters
-{
-    public class PaginationFilter
-    {
-        public PaginationFilter() { }
-        public PaginationFilter(int pageNumber, int pageSize)
-        {
-            PageNumber = pageNumber;
-            PageSize = pageSize > 25 ? 25: pageSize ;
-        }
+﻿namespace DRApplication.Shared.Filters;
 
-        public int PageNumber { get; set; } = 1;
-        public int PageSize { get; set; } = 10;
+public class PaginationFilter
+{
+    public PaginationFilter() { }
+    public PaginationFilter(int pageNumber, int pageSize)
+    {
+        PageNumber = pageNumber;
+        PageSize = pageSize > 25 ? 25: pageSize ;
     }
+
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
 }
