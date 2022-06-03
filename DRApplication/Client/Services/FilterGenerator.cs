e68927d030794d6398cc116447b86c1e40740dd9
Kevin Morgan <kmorgan26@gmail.com>
2022-06-03 15:24:04 -0500

    Add Version LoadList Component

---
+++ DRApplication/Client/Services/FilterGenerator.cs
@@ -0,0 +1,27 @@
+﻿using DRApplication.Shared.Enums;
+using DRApplication.Shared.Filters;
+
+namespace DRApplication.Client.Services
+{
+    public class FilterGenerator<TEntity> where TEntity : class
+    {
+        public async Task<QueryFilter<TEntity>> GetFilterForPropertyByNameAsync(string propertyName, int id)
+        {
+            var filter = new QueryFilter<TEntity>();
+            var filterProperties = new List<FilterProperty>();
+            filterProperties.Add(new FilterProperty()
+            { 
+                Name = propertyName,
+                Value = id.ToString(),
+                Operator = FilterQueryOperator.Equals
+            });
+            filter.OrderByDescending = true;
+            filter.OrderByPropertyName = "Id";
+            filter.PaginationFilter = null;
+
+            filter.FilterProperties = filterProperties;
+            
+            return filter;
+        }
+    }
+}
