8c2683869f67cb6f564c5edddbb6755a59d2d420
Kevin Morgan <kmorgan26@gmail.com>
2022-06-03 15:27:11 -0500

    Minor cleanup

--- DRApplication/Client/ViewModels/ConfigurationViewModels/VersionLoads/VersionsLoadVm.cs
+++ DRApplication/Client/ViewModels/ConfigurationViewModels/VersionLoads/VersionsLoadVm.cs
@@ -5,4 +5,7 @@ public class VersionsLoadVm
     public int Id { get; set; }
     public int LoadId { get; set; }
     public int SoftwareVersionId { get; set; }
+    
+    public string SoftwareVersionName { get; set; }
+
 }
