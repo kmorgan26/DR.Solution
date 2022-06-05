fd197949abdbc0b8c95a174831b4f217415e1c21
Kevin Morgan <kmorgan26@gmail.com>
2022-06-05 06:19:41 -0500

    Add method

--- DRApplication/Client/Services/FilterGenerator.cs
+++ DRApplication/Client/Services/FilterGenerator.cs
@@ -23,5 +23,25 @@ namespace DRApplication.Client.Services
             
             return filter;
         }
+        public async Task<QueryFilter<TEntity>> GetFilterForPropertiesByNamesAsync(string properyName, List<string> ids)
+        {
+            var filter = new QueryFilter<TEntity>();
+            var filterProperties = new List<FilterProperty>();
+            foreach (var i in ids)
+            {
+                filterProperties.Add(new FilterProperty()
+                {
+                    Name = "SoftwareSystemId",
+                    Value = i,
+                    Operator = FilterQueryOperator.Equals
+                });
+            }
+            filter.OrderByDescending = true;
+            filter.OrderByPropertyName = "Id";
+            filter.PaginationFilter = null;
+            filter.FilterProperties = filterProperties;
+            return filter;
+        }
     }
+    
 }
