0a4b6d2c648274b9364e730ef13493fa37eb171b
Kevin.D.Morgan <Kevin.D.Morgan@Gdit.com>
2022-06-06 15:50:44 -0500

    Implement AppState Service

--- DRApplication/Client/Controls/Configurations/HardwareSystems/HardwareSystemList.razor.cs
+++ DRApplication/Client/Controls/Configurations/HardwareSystems/HardwareSystemList.razor.cs
@@ -1,15 +1,23 @@
-using DRApplication.Client.Services;
 using DRApplication.Client.ViewModels;
+using DRApplication.Client.Services;
 
 namespace DRApplication.Client.Controls
 {
     public partial class HardwareSystemList
     {
-        List<HardwareSystemVm> _hardwareSystemVms = new();
-        protected override async Task OnInitializedAsync()
+        bool _isBusy;
+        async Task SetHardwareSystems()
         {
             var items = await manager.GetAllAsync();
-            _hardwareSystemVms = Mapping.Mapper.Map<List<HardwareSystemVm>>(items);
+            var vms = Mapping.Mapper.Map<List<HardwareSystemVm>>(items);
+            AppState.UpdateHardwareSystemVms(this, vms);
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            _isBusy = true;
+            await SetHardwareSystems();
+            _isBusy = false;
         }
     }
 }
