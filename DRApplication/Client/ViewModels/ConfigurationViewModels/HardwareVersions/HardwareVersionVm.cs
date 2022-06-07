7c4bfec44917915da8945d91d8476d7e2a0bdcc3
Kevin Morgan <kmorgan26@gmail.com>
2022-06-07 12:07:50 -0500

    Fixed HardwareVersion Edit

--- DRApplication/Client/ViewModels/ConfigurationViewModels/HardwareVersions/HardwareVersionVm.cs
+++ DRApplication/Client/ViewModels/ConfigurationViewModels/HardwareVersions/HardwareVersionVm.cs
@@ -3,12 +3,12 @@ namespace DRApplication.Client.ViewModels;
 
 public class HardwareVersionVm
 {
-    public int Id { get; set; }
+    public int Id { get; set; } = 0;
     
     public string Name { get; set; } = string.Empty;
+
+    public int HardwareSystemId { get; set; } = 0;
     
-    public int HardwareSystemId { get; set; }
-    
-    public DateTime VersionDate { get; set; }
+    public DateTime? VersionDate { get; set; }
 
 }
