12e4549a7bc8f29b39e026e1df09b5fd8e4449ee
Kevin Morgan <kmorgan26@gmail.com>
2022-06-08 07:53:36 -0500

    Add SoftwareService

---
+++ DRApplication/Client/Interfaces/Configuration/ISoftwareService.cs
@@ -0,0 +1,9 @@
+﻿using DRApplication.Client.ViewModels;
+
+namespace DRApplication.Client.Interfaces
+{
+    public interface ISoftwareService
+    {
+        Task<IEnumerable<SoftwareSystemVm>> GetSoftwareSystemsByHardwareConfigId(int id);
+    }
+}
---
+++ DRApplication/Client/Services/Configurations/SoftwareService.cs
@@ -0,0 +1,22 @@
+﻿using DRApplication.Client.Interfaces;
+using DRApplication.Client.ViewModels;
+using DRApplication.Shared.Models.ConfigurationModels;
+using AutoMapper;
+
+namespace DRApplication.Client.Services;
+
+public class SoftwareService : ISoftwareService
+{
+    private readonly SoftwareSystemManager _softwareSystemManager;
+
+    public SoftwareService(SoftwareSystemManager softwareSystemManager)
+    {
+        _softwareSystemManager = softwareSystemManager;
+    }
+    public async Task<IEnumerable<SoftwareSystemVm>> GetSoftwareSystemsByHardwareConfigId(int id)
+    {
+        var systems = await _softwareSystemManager.GetAllAsync();
+        var filterd = systems.Where(x => x.HardwareConfigId == id);
+        return Mapping.Mapper.Map<IEnumerable<SoftwareSystemVm>>(filterd);
+    }
+}
