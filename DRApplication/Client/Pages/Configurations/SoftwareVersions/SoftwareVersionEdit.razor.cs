7e6acaf1683146527d20670da5891c4ee00413bf
Kevin Morgan <kmorgan26@gmail.com>
2022-06-08 08:26:47 -0500

    Update Software Pages

--- DRApplication/Client/Pages/Configurations/SoftwareVersions/SoftwareVersionEdit.razor.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.AspNetCore.Components;
-namespace DRApplication.Client.Pages;
-public partial class SoftwareVersionEdit
-{
-    [Parameter]
-    public int SoftwareVersionId { get; set; }
-}
