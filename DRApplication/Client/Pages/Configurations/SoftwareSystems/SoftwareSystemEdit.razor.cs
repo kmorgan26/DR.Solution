d39ce50e1a290a10bf1e0921a4e03aacbc7e3c88
Kevin Morgan <kmorgan26@gmail.com>
2022-06-08 06:02:47 -0500

    Update Software System Pages

--- DRApplication/Client/Pages/Configurations/SoftwareSystems/SoftwareSystemEdit.razor.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Components;
-
-namespace DRApplication.Client.Pages;
-
-public partial class SoftwareSystemEdit
-{
-    [Parameter]
-    public int SoftwareSystemId { get; set; }
-}
