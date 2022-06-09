dc00359105f08717b78cce9c04e4927e5e41d71c
Kevin Morgan <kmorgan26@gmail.com>
2022-06-09 16:47:05 -0500

    Add CurrentLoadManager

---
+++ DRApplication/Client/Services/Configurations/CurrentLoadManager.cs
@@ -0,0 +1,13 @@
+﻿using DRApplication.Shared.Models.ConfigurationModels;
+
+namespace DRApplication.Client.Services;
+
+public class CurrentLoadManager : ApiRepository<CurrentLoad>
+{
+    private readonly HttpClient _http;
+
+    public CurrentLoadManager(HttpClient http) : base(http, "currentload", "Id")
+    {
+        _http = http;
+    }
+}
