0a1d409d34c9010c6bf50135987541bf57c5e98e
Kevin Morgan <kmorgan26@gmail.com>
2022-06-09 15:27:49 -0500

    Add Current Loads Table

---
+++ DRApplication.Database/dbo/Tables/CurrentLoads.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[CurrentLoads] (
+    [Id]       INT IDENTITY (1, 1) NOT NULL,
+    [LoadId]   INT NOT NULL,
+    [DeviceId] INT NOT NULL,
+    CONSTRAINT [PK_CurrentLoads] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
