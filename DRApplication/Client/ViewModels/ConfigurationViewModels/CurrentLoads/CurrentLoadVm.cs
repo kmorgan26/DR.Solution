1e8e03fcd27b72e525dc08a6a2edf507b4a15c8b
Kevin Morgan <kmorgan26@gmail.com>
2022-06-09 16:46:15 -0500

    Add CurrentLoad View Models

---
+++ DRApplication/Client/ViewModels/ConfigurationViewModels/CurrentLoads/CurrentLoadVm.cs
@@ -0,0 +1,9 @@
+﻿namespace DRApplication.Client.ViewModels
+{
+    public class CurrentLoadVm
+    {
+        public int Id { get; set; }
+        public string LoadName { get; set; } = string.Empty;
+        public int DeviceId { get; set; } = 0;
+    }
+}
