7158a973e7ba99f09de79719eaa767c3ad5daf1a
Kevin.D.Morgan <Kevin.D.Morgan@Gdit.com>
2022-06-13 16:42:00 -0500

    Cleanup old files

--- DRApplication/Client/Program.cs
+++ DRApplication/Client/Program.cs
@@ -41,11 +41,8 @@ builder.Services.AddScoped<AppState>();
 builder.Services.AddTransient<IPlatformService, PlatformService>();
 builder.Services.AddTransient<ILoadBuilderService, LoadBuilderService>();
 builder.Services.AddTransient<IGenericListService, GenericListService>();
-builder.Services.AddTransient<IForeignKeyListService, ForeignKeyListService>();
 builder.Services.AddTransient<IHardwareService, HardwareService>();
 builder.Services.AddTransient<ISoftwareService, SoftwareService>();
-
-builder.Services.AddTransient(typeof(ITableService<>), typeof(TableService<>));
 
 builder.Services.AddBlazoredLocalStorage();
 
