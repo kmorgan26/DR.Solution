53ce5bd5325b042a85c6e11b70dfcf68f8e773c7
Kevin Morgan <kmorgan26@gmail.com>
2022-06-13 06:30:34 -0500

    Add Test project and remove boilerplate

---
+++ DRApplication.TestProject/Buttons/SubmitButtonTests.cs
@@ -0,0 +1,41 @@
+﻿using DRApplication.Client.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRApplication.TestProject
+{
+    public class SubmitButtonTests : TestContext
+    {
+        [Fact]
+        public void ButtonHasUrl()
+        {
+            //arrange
+            var cut = RenderComponent<CancelButton>(parameters => parameters
+            .Add(p => p.Url, "myurl"));
+            
+            //act
+            var renderedMarkup = cut.Markup;
+
+            //assert
+            Assert.Contains("href=\"myurl\"", renderedMarkup);
+        }
+
+        [Fact]
+        public void ButtonNavigatesToUrl()
+        {
+            //arrange
+            var cut = RenderComponent<CancelButton>(parameters => parameters
+            .Add(p => p.Url, "myurl"));
+
+            //act
+            var renderedMarkup = cut.Markup;
+            cut.Find("a").Click();
+
+            Assert.
+            
+        }
+    }
+}
