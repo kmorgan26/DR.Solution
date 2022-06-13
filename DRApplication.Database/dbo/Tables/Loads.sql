af9658963057c8dea61aeab1f40cc81abb69d01b
Kevin.D.Morgan <Kevin.D.Morgan@Gdit.com>
2022-06-13 10:47:18 -0500

    Add IsAccepted Property

--- DRApplication.Database/dbo/Tables/Loads.sql
+++ DRApplication.Database/dbo/Tables/Loads.sql
@@ -2,6 +2,7 @@
     [Id]           INT          IDENTITY (1, 1) NOT NULL,
     [Name]         VARCHAR (50) NOT NULL,
     [DeviceTypeId] INT          NOT NULL,
+    [IsAccepted] BIT NOT NULL, 
     CONSTRAINT [PK_Loads] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
