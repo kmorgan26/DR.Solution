169b20fefd3ad024e238a8679171ded2fc3355ec
Kevin Morgan <kmorgan26@gmail.com>
2022-06-14 06:20:36 -0500

    Change Load Table. Replaced DeviceTypeId with HardwareConfigId

--- DRApplication.Database/dbo/Tables/Loads.sql
+++ DRApplication.Database/dbo/Tables/Loads.sql
@@ -1,10 +1,13 @@
 ﻿CREATE TABLE [dbo].[Loads] (
-    [Id]           INT          IDENTITY (1, 1) NOT NULL,
-    [Name]         VARCHAR (50) NOT NULL,
-    [DeviceTypeId] INT          NOT NULL,
-    [IsAccepted]   BIT          NOT NULL,
-    CONSTRAINT [PK_Loads] PRIMARY KEY CLUSTERED ([Id] ASC)
+    [Id]               INT          IDENTITY (1, 1) NOT NULL,
+    [Name]             VARCHAR (50) NOT NULL,
+    [HardwareConfigId] INT          NOT NULL,
+    [IsAccepted]       BIT          NOT NULL,
+    CONSTRAINT [PK_Loads] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Loads_HardwareConfigs] FOREIGN KEY ([HardwareConfigId]) REFERENCES [dbo].[HardwareConfigs] ([Id])
 );
+
+
 
 
 
