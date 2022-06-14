ded8f5e51e9423236c757a0c60af0edf35bcbb1d
Kevin Morgan <kmorgan26@gmail.com>
2022-06-14 06:11:10 -0500

    format of loads table definition

--- DRApplication.Database/dbo/Tables/Loads.sql
+++ DRApplication.Database/dbo/Tables/Loads.sql
@@ -2,7 +2,9 @@
     [Id]           INT          IDENTITY (1, 1) NOT NULL,
     [Name]         VARCHAR (50) NOT NULL,
     [DeviceTypeId] INT          NOT NULL,
-    [IsAccepted] BIT NOT NULL, 
+    [IsAccepted]   BIT          NOT NULL,
     CONSTRAINT [PK_Loads] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
