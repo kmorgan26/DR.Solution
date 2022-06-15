bcd1caed10fceefb75f724f4d8db359d572ba21e
Kevin.D.Morgan <Kevin.D.Morgan@Gdit.com>
2022-06-15 13:06:48 -0500

    Add CurrentLoadName property

---
+++ DRApplication/Client/ViewModels/Platform/DeviceViewModels/DeviceCardVm.cs
@@ -0,0 +1,18 @@
+﻿namespace DRApplication.Client.ViewModels.Platform.DeviceViewModels
+{
+    public class DeviceCardVm
+    {
+        public int Id { get; set; }
+
+        public string Device { get; set; } = string.Empty;
+
+        public string Platform { get; set; } = string.Empty;
+
+        public int DeviceTypeId { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public string CurrentLoadName { get; set; } = string.Empty;
+
+    }
+}
