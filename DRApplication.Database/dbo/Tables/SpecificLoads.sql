3e33556f5f0828a2df59c0e2ed1b059a2ff5ab25
Kevin Morgan <kmorgan26@gmail.com>
2022-06-16 06:54:33 -0500

    Add Table for Specific Loads

---
+++ DRApplication.Database/dbo/Tables/SpecificLoads.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[SpecificLoads] (
+    [Id]       INT IDENTITY (1, 1) NOT NULL,
+    [LoadId]   INT NOT NULL,
+    [DeviceId] INT NOT NULL,
+    CONSTRAINT [PK_SpecificLoad] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_SpecificLoads_Devices] FOREIGN KEY ([DeviceId]) REFERENCES [dbo].[Devices] ([Id]),
+    CONSTRAINT [FK_SpecificLoads_Loads] FOREIGN KEY ([LoadId]) REFERENCES [dbo].[Loads] ([Id])
+);
+
