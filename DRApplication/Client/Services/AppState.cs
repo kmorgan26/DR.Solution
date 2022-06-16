0e21d8440d24d38cb292439112bbbc849fadb447
Kevin Morgan <kmorgan26@gmail.com>
2022-06-16 11:23:10 -0500

    Added properties for SpecificLoad and arranged properties in class

--- DRApplication/Client/Services/AppState.cs
+++ DRApplication/Client/Services/AppState.cs
@@ -8,70 +8,59 @@ namespace DRApplication.Client.Services
         #region -- View Model Properties --
 
         public CurrentLoadVm CurrentLoadVm { get; private set; } = new();
-
+        
+        public DeviceVm DeviceVm { get; private set; } = new();
+        
+        public DeviceTypeVm DeviceTypeVm { get; private set; } = new();
+        
+        public HardwareConfigVm HardwareConfigVm { get; private set; } = new();
+        
         public HardwareSystemVm HardwareSystemVm { get; private set; } = new();
-
+        
         public HardwareVersionVm HardwareVersionVm { get; private set; } = new();
 
         public HardwareVersionsConfigVm HardwareVersionsConfigVm { get; private set; } = new();
         
-        public HardwareConfigVm HardwareConfigVm { get; private set; } = new();
+        public LoadVm LoadVm { get; private set; } = new();
+        
+        public MaintainerVm MaintainerVm { get; private set; } = new();
 
         public SoftwareSystemVm SoftwareSystemVm { get; private set; } = new();
 
         public SoftwareVersionVm SoftwareVersionVm { get; private set; } = new();
-
-        public LoadVm LoadVm { get; private set; } = new();
+        
+        public SpecificLoadVm SpecificLoadVm { get; private set; } = new();
 
         public VersionsLoadVm VersionsLoadVm { get; private set; } = new();
-
-        public DeviceTypeVm DeviceTypeVm { get; private set; } = new();
-
-        public DeviceVm DeviceVm { get; private set; } = new();
-
-        public MaintainerVm MaintainerVm { get; private set; } = new();
 
         #endregion
 
         #region -- View Model Collections --
         public IEnumerable<CurrentLoadVm> CurrentLoadVms { get; private set; } = new List<CurrentLoadVm>();
+
+        public IEnumerable<DeviceTypeVm> DeviceTypeVms { get; private set; } = new List<DeviceTypeVm>();
+        
+        public IEnumerable<DeviceVm> DeviceVms { get; private set; } = new List<DeviceVm>();
+        
+        public IEnumerable<HardwareConfigVm> HardwareConfigVms { get; private set; } = new List<HardwareConfigVm>();
         
         public IEnumerable<HardwareSystemVm> HardwareSystemVms { get; private set; } = new List<HardwareSystemVm>();
 
         public IEnumerable<HardwareVersionVm> HardwareVersionVms { get; private set; } = new List<HardwareVersionVm>();
 
         public IEnumerable<HardwareVersionsConfigVm> HardwareVersionsConfigVms { get; private set; } = new List<HardwareVersionsConfigVm>();
+        
+        public IEnumerable<LoadVm> LoadVms { get; private set; } = new List<LoadVm>();
 
-        public IEnumerable<HardwareConfigVm> HardwareConfigVms { get; private set; } = new List<HardwareConfigVm>();
+        public IEnumerable<MaintainerVm> MaintainerVms { get; private set; } = new List<MaintainerVm>();
 
         public IEnumerable<SoftwareSystemVm> SoftwareSystemVms { get; private set; } = new List<SoftwareSystemVm>();
         
         public IEnumerable<SoftwareVersionVm> SoftwareVersionVms { get; private set; } = new List<SoftwareVersionVm>();
-        
-        public IEnumerable<LoadVm> LoadVms { get; private set; } = new List<LoadVm>();
+
+        public IEnumerable<SpecificLoadVm> SpecificLoadVms { get; private set; } = new List<SpecificLoadVm>();
         
         public IEnumerable<VersionsLoadVm> VersionsLoadVms { get; private set; } = new List<VersionsLoadVm>();
-        
-        public IEnumerable<DeviceTypeVm> DeviceTypeVms { get; private set; } = new List<DeviceTypeVm>();
-
-        public IEnumerable<DeviceVm> DeviceVms { get; private set; } = new List<DeviceVm>();
-
-        public IEnumerable<MaintainerVm> MaintainerVms { get; private set; } = new List<MaintainerVm>();
-
-        #endregion
-
-        #region -- single ID Methods --
-
-        //public void UpdateHardwareConfigId(ComponentBase Source, int Model)
-        //{
-        //    HardwareConfigId = Model;
-        //    NotifyStateChanged(Source, "HardwareConfigId");
-        //}
-        //public void UpdateDeviceTypeId(ComponentBase Source, int Model)
-        //{
-        //    DeviceTypeId = Model;
-        //    NotifyStateChanged(Source, "DeviceTypeId");
-        //}
 
         #endregion
 
@@ -81,6 +70,21 @@ namespace DRApplication.Client.Services
         {
             this.CurrentLoadVm = Model;
             NotifyStateChanged(Source, "CurrentLoadVm");
+        }
+        public void UpdateDeviceVm(ComponentBase Source, DeviceVm Model)
+        {
+            this.DeviceVm = Model;
+            NotifyStateChanged(Source, "DeviceVm");
+        }
+        public void UpdateDeviceTypeVm(ComponentBase Source, DeviceTypeVm Model)
+        {
+            this.DeviceTypeVm = Model;
+            NotifyStateChanged(Source, "DeviceTypeVm");
+        }
+        public void UpdateHardwareConfigVm(ComponentBase Source, HardwareConfigVm Model)
+        {
+            this.HardwareConfigVm = Model;
+            NotifyStateChanged(Source, "HardwareConfigVm");
         }
         public void UpdateHardwareSystemVm(ComponentBase Source, HardwareSystemVm Model)
         {
@@ -97,10 +101,15 @@ namespace DRApplication.Client.Services
             this.HardwareVersionsConfigVm = Model;
             NotifyStateChanged(Source, "HardwareVersionsConfigVm");
         }
-        public void UpdateHardwareConfigVm(ComponentBase Source, HardwareConfigVm Model)
+        public void UpdateLoadVm(ComponentBase Source, LoadVm Model)
         {
-            this.HardwareConfigVm = Model;
-            NotifyStateChanged(Source, "HardwareConfigVm");
+            this.LoadVm = Model;
+            NotifyStateChanged(Source, "LoadVm");
+        }
+        public void UpdateMaintainerVm(ComponentBase Source, MaintainerVm Model)
+        {
+            this.MaintainerVm = Model;
+            NotifyStateChanged(Source, "MaintainerVm");
         }
         public void UpdateSoftwareSystemVm(ComponentBase Source, SoftwareSystemVm Model)
         {
@@ -112,30 +121,15 @@ namespace DRApplication.Client.Services
             this.SoftwareVersionVm = Model;
             NotifyStateChanged(Source, "SoftwareVersionVm");
         }
+        public void UpdateSpecificLoadVm(ComponentBase Source, SpecificLoadVm Model)
+        {
+            this.SpecificLoadVm = Model;
+            NotifyStateChanged(Source, "SpecificLoadVm");
+        }
         public void UpdateVersionsLoadVm(ComponentBase Source, VersionsLoadVm Model)
         {
             this.VersionsLoadVm = Model;
             NotifyStateChanged(Source, "VersionsLoadVm");
-        }
-        public void UpdateLoadVm(ComponentBase Source, LoadVm Model)
-        {
-            this.LoadVm = Model;
-            NotifyStateChanged(Source, "LoadVm");
-        }
-        public void UpdateDeviceTypeVm(ComponentBase Source, DeviceTypeVm Model)
-        {
-            this.DeviceTypeVm = Model;
-            NotifyStateChanged(Source, "DeviceTypeVm");
-        }
-        public void UpdateDeviceVm(ComponentBase Source, DeviceVm Model)
-        {
-            this.DeviceVm = Model;
-            NotifyStateChanged(Source, "DeviceVm");
-        }
-        public void UpdateMaintainerVm(ComponentBase Source, MaintainerVm Model)
-        {
-            this.MaintainerVm = Model;
-            NotifyStateChanged(Source, "MaintainerVm");
         }
 
         #endregion
@@ -146,6 +140,21 @@ namespace DRApplication.Client.Services
         {
             this.CurrentLoadVms = Model;
             NotifyStateChanged(Source, "CurrentLoadVms");
+        }
+        public void UpdateDeviceVms(ComponentBase Source, IEnumerable<DeviceVm> Model)
+        {
+            this.DeviceVms = Model;
+            NotifyStateChanged(Source, "DeviceVms");
+        }
+        public void UpdateDeviceTypeVms(ComponentBase Source, IEnumerable<DeviceTypeVm> Model)
+        {
+            this.DeviceTypeVms = Model;
+            NotifyStateChanged(Source, "DeviceTypeVms");
+        }
+        public void UpdateHardwareConfigVms(ComponentBase Source, IEnumerable<HardwareConfigVm> Model)
+        {
+            this.HardwareConfigVms = Model;
+            NotifyStateChanged(Source, "HardwareConfigVms");
         }
         public void UpdateHardwareSystemVms(ComponentBase Source, IEnumerable<HardwareSystemVm> Model)
         {
@@ -162,10 +171,15 @@ namespace DRApplication.Client.Services
             this.HardwareVersionsConfigVms = Model;
             NotifyStateChanged(Source, "HardwareVersionsConfigVms");
         }
-        public void UpdateHardwareConfigVms(ComponentBase Source, IEnumerable<HardwareConfigVm> Model)
+        public void UpdateLoadVms(ComponentBase Source, IEnumerable<LoadVm> Model)
         {
-            this.HardwareConfigVms = Model;
-            NotifyStateChanged(Source, "HardwareConfigVms");
+            this.LoadVms = Model;
+            NotifyStateChanged(Source, "LoadVms");
+        }
+        public void UpdateMaintainerVms(ComponentBase Source, IEnumerable<MaintainerVm> Model)
+        {
+            this.MaintainerVms = Model;
+            NotifyStateChanged(Source, "MaintainerVms");
         }
         public void UpdateSoftwareSystemVms(ComponentBase Source, IEnumerable<SoftwareSystemVm> Model)
         {
@@ -177,31 +191,17 @@ namespace DRApplication.Client.Services
             this.SoftwareVersionVms = Model;
             NotifyStateChanged(Source, "SoftwareVersionVms");
         }
+        public void UpdateSpecificLoadVms(ComponentBase Source, IEnumerable<SpecificLoadVm> Model)
+        {
+            this.SpecificLoadVms = Model;
+            NotifyStateChanged(Source, "SpecificLoadVms");
+        }
         public void UpdateVersionsLoadVms(ComponentBase Source, IEnumerable<VersionsLoadVm> Model)
         {
             this.VersionsLoadVms = Model;
             NotifyStateChanged(Source, "VersionsLoadVms");
         }
-        public void UpdateLoadVms(ComponentBase Source, IEnumerable<LoadVm> Model)
-        {
-            this.LoadVms = Model;
-            NotifyStateChanged(Source, "LoadVms");
-        }
-        public void UpdateDeviceTypeVms(ComponentBase Source, IEnumerable<DeviceTypeVm> Model)
-        {
-            this.DeviceTypeVms = Model;
-            NotifyStateChanged(Source, "DeviceTypeVms");
-        }
-        public void UpdateDeviceVms(ComponentBase Source, IEnumerable<DeviceVm> Model)
-        {
-            this.DeviceVms = Model;
-            NotifyStateChanged(Source, "DeviceVms");
-        }
-        public void UpdateMaintainerVms(ComponentBase Source, IEnumerable<MaintainerVm> Model)
-        {
-            this.MaintainerVms = Model;
-            NotifyStateChanged(Source, "MaintainerVms");
-        }
+        
         #endregion
 
         public event Action<ComponentBase, string> StateChanged;
