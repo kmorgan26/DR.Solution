10923a0274b6df19bb40c6e440d7d8458c3c9401
Kevin D. Morgan <kevin.d.morgan@gdit.com>
2022-06-23 16:09:43 -0500

    Add DR, Issue, and MaintIssue Managers

---
+++ DRApplication/Client/Services/Issues/Issues/Managers/IssueManager.cs
@@ -0,0 +1,12 @@
+﻿using DRApplication.Shared.Models;
+namespace DRApplication.Client.Services;
+
+public class IssueManager : ApiRepository<Issue>
+{
+    private readonly HttpClient _http;
+
+    public IssueManager(HttpClient http) : base(http, "issue", "Id")
+    {
+        _http = http;
+    }
+}
---
+++ DRApplication/Client/Services/Issues/MaintIssues/Managers/MaintIssueManager.cs
@@ -0,0 +1,12 @@
+﻿using DRApplication.Shared.Models;
+namespace DRApplication.Client.Services;
+
+public class MaintIssueManager : ApiRepository<MaintIssue>
+{
+    private readonly HttpClient _http;
+
+    public MaintIssueManager(HttpClient http) : base(http, "maintissue", "Id")
+    {
+        _http = http;
+    }
+}
