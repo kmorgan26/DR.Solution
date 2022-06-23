e2d4f4b4afa69f47c2aed92771b4e555231caebf
Kevin D. Morgan <kevin.d.morgan@gdit.com>
2022-06-23 15:38:09 -0500

    Add Maint Issue View Models

---
+++ DRApplication/Client/ViewModels/Issues/Maint/MaintenanceIssueInsertVm.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DRApplication.Client.ViewModels;
+public class MaintenanceIssueInsertVm
+{
+    public int DrTypeId { get; set; }
+    public int SimStatusId { get; set; } = 0;
+
+    [Required]
+    public string Description { get; set; } = string.Empty;
+
+    [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+    public DateTime? IssueDate { get; set; } = DateTime.Today;
+
+    public string EnteredBy { get; set; } = string.Empty;
+
+    [Required]
+    public int DeviceId { get; set; }
+
+    [Required]
+    public string Pid { get; set; } = string.Empty;
+
+
+    public int CorrectiveActionId { get; set; } = 0;
+}
---
+++ DRApplication/Client/ViewModels/Issues/Maint/MaintenanceIssueVm.cs
@@ -0,0 +1,14 @@
+﻿namespace DRApplication.Client.ViewModels;
+public class MaintenanceIssueVm
+{
+    public int DrTypeId { get; set; }
+    public int SimStatusId { get; set; } = 0;
+    public string Description { get; set; } = string.Empty;
+    public DateTime IssueDate { get; set; }
+    public string EnteredBy { get; set; } = string.Empty;
+
+
+    public string Pid { get; set; } = string.Empty;
+    public int CorrectiveActionId { get; set; } = 0;
+
+}
