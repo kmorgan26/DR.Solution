a7d4cdf9ee41b1240437a0854ae989c04213dec3
Kevin Morgan <kmorgan26@gmail.com>
2022-06-24 13:47:40 -0500

    Add IssueInsertVm

---
+++ DRApplication/Client/ViewModels/Issues/Issue/IssueInsertVm.cs
@@ -0,0 +1,9 @@
+﻿namespace DRApplication.Client.ViewModels;
+public class IssueInsertVm
+{
+    public int DrTypeId { get; set; } = 0;
+    public int SimStatusId { get; set; } = 0;
+    public string Description { get; set; } = string.Empty;
+    public DateTime IssueDate { get; set; }
+    public string EnteredBy { get; set; } = string.Empty;
+}
