f15f6821a81118d4a308a26f37b914e9785759be
Kevin Morgan <kmorgan26@gmail.com>
2022-06-24 13:46:46 -0500

    Created ManagerService

---
+++ DRApplication/Client/Services/ManagerService.cs
@@ -0,0 +1,37 @@
+﻿namespace DRApplication.Client.Services
+{
+    public class ManagerService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ManagerService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public DeviceManager DeviceManager()
+        {
+            return new DeviceManager(_httpClient);
+        }
+        public CorrectiveActionManager CorrectiveActionManager()
+        {
+            return new CorrectiveActionManager(_httpClient);
+        }
+        public DrManager DrManager()
+        {
+            return new DrManager(_httpClient);
+        }
+        public MaintIssueManager MaintIssueManager()
+        {
+            return new MaintIssueManager(_httpClient);
+        }
+        public IssueManager IssueManager()
+        {
+            return new IssueManager(_httpClient);
+        }
+        public DeviceDiscoveredManager DeviceDiscoveredManager()
+        {
+            return new DeviceDiscoveredManager(_httpClient);
+        }
+
+    }
+}
