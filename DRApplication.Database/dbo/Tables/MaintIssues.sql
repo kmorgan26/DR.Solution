ed6fd95ab5cd389c00528924811b281b3f8769b6
Kevin Morgan <kmorgan26@gmail.com>
2022-06-24 10:04:11 -0500

    Add Property for Action Taken

--- DRApplication.Database/dbo/Tables/MaintIssues.sql
+++ DRApplication.Database/dbo/Tables/MaintIssues.sql
@@ -3,8 +3,11 @@
     [IssueId]            INT           NOT NULL,
     [Pid]                NVARCHAR (10) NOT NULL,
     [CorrectiveActionId] INT           NOT NULL,
+    [ActionTaken]        VARCHAR (50)  NOT NULL,
     CONSTRAINT [PK_MaintIssues] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_MaintIssues_CorrectiveActions] FOREIGN KEY ([CorrectiveActionId]) REFERENCES [dbo].[CorrectiveActions] ([Id]),
     CONSTRAINT [FK_MaintIssues_Issues] FOREIGN KEY ([IssueId]) REFERENCES [dbo].[Issues] ([Id])
 );
+
+
 
