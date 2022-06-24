e19db1dd8d3bac8fe2877dcc606610963c0c940f
Kevin Morgan <kmorgan26@gmail.com>
2022-06-24 13:45:34 -0500

    Modify Interface with parameters for methods

--- DRApplication/Client/Interfaces/Issues/IIssueService.cs
+++ DRApplication/Client/Interfaces/Issues/IIssueService.cs
@@ -3,9 +3,10 @@ namespace DRApplication.Client.Interfaces;
 
 public interface IIssueService
 {
-    Task<int> InsertIssueAsync();
+    Task<int> InsertIssueAsync(IssueInsertVm issueInsertVm);
     Task<int> InsertMaintenanceIssueAsync(MaintenanceIssueInsertVm maintenanceIssueInsertVm);
     Task<int> InsertDrAsync();
+    Task<int> InsertDeviceDiscoveredAsync(int deviceId, int issueId);
     Task<IEnumerable<CorrectiveActionVm>> GetCorrectiveActionVmsAsync();
 
 }
