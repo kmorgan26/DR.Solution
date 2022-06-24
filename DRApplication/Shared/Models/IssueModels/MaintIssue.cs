e0eae9480b7b74f67a626301fa4a5d20941340c2
Kevin Morgan <kmorgan26@gmail.com>
2022-06-24 10:09:06 -0500

    Add Action Taken Property

--- DRApplication/Shared/Models/IssueModels/MaintIssue.cs
+++ DRApplication/Shared/Models/IssueModels/MaintIssue.cs
@@ -5,5 +5,6 @@ public class MaintIssue
     public int Id { get; set; }
     public int IssueId { get; set; }
     public string Pid { get; set; } = string.Empty;
+    public string ActionTaken { get; set; } = string.Empty;
     public int CorrectiveActionId { get; set; }
 }
