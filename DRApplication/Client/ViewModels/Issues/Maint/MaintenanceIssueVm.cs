77717a9518236e6d0e4b2fd5c8657963028cde66
Kevin Morgan <kmorgan26@gmail.com>
2022-06-24 16:38:20 -0500

    Modify MaintIssueVm

--- DRApplication/Client/ViewModels/Issues/Maint/MaintenanceIssueVm.cs
+++ DRApplication/Client/ViewModels/Issues/Maint/MaintenanceIssueVm.cs
@@ -1,14 +1,32 @@
-﻿namespace DRApplication.Client.ViewModels;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DRApplication.Client.ViewModels;
 public class MaintenanceIssueVm
 {
-    public int DrTypeId { get; set; }
-    public int SimStatusId { get; set; } = 0;
+    public int IssueId { get; set; } = 0;
+
+    [Display(Name="Type")]
+    public string DrType { get; set; } = string.Empty;
+    
+    [Display(Name="Status")]
+    public string SimStatus { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
+
+    [Display(Name="Issue Date")]
     public DateTime IssueDate { get; set; }
+
+    [Display(Name ="Entered By")]
     public string EnteredBy { get; set; } = string.Empty;
 
-
+    [Display(Name ="PID")]
     public string Pid { get; set; } = string.Empty;
-    public int CorrectiveActionId { get; set; } = 0;
+
+    public string Device { get; set; } = string.Empty;
+
+    [Display(Name ="Corrective Action")]
+    public string CorrectiveAction { get; set; } = string.Empty;
+
+    [Display(Name ="Action Taken")]
+    public string ActionTaken { get; set; } = string.Empty;
 
 }
