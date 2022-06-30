6dc66f187eb9fe9547a719125f46a80aed4d59b3
Kevin Morgan <kmorgan26@gmail.com>
2022-06-30 16:19:30 -0500

    Change Maintainer.Name to varchar(25)

--- DRApplication.Database/dbo/Tables/Maintainers.sql
+++ DRApplication.Database/dbo/Tables/Maintainers.sql
@@ -1,6 +1,8 @@
 ﻿CREATE TABLE [dbo].[Maintainers] (
-    [Id]   INT            IDENTITY (1, 1) NOT NULL,
-    [Name] NVARCHAR (255) NOT NULL,
+    [Id]   INT          IDENTITY (1, 1) NOT NULL,
+    [Name] VARCHAR (25) NOT NULL,
     CONSTRAINT [PK_Maintainers] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
