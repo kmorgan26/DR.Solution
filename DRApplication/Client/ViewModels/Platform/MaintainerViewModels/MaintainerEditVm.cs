0b6e7373ed3bf024b0ac59fef49684723ba1466e
Kevin Morgan <kmorgan26@gmail.com>
2022-06-30 15:53:42 -0500

    Remove Data Annotations

--- DRApplication/Client/ViewModels/Platform/MaintainerViewModels/MaintainerEditVm.cs
+++ DRApplication/Client/ViewModels/Platform/MaintainerViewModels/MaintainerEditVm.cs
@@ -5,9 +5,5 @@ namespace DRApplication.Client.ViewModels;
 public class MaintainerEditVm
 {
     public int Id { get; set; }
-
-    [Required]
-    [Range(1, 255, ErrorMessage = "Maintainer Name cannot be more than 255 characters long")]
     public string Name { get; set; } = null!;
-
 }
