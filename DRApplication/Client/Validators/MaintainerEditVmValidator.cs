8caddce0a4c3e14cfc88599bf0448699214834ea
Kevin Morgan <kmorgan26@gmail.com>
2022-06-30 20:29:03 -0500

    Change namespace

--- DRApplication/Client/Validators/MaintainerEditVmValidator.cs
+++ DRApplication/Client/Validators/MaintainerEditVmValidator.cs
@@ -1,24 +1,17 @@
 ﻿using DRApplication.Client.ViewModels;
 using FluentValidation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DRApplication.Shared.Validators
+namespace DRApplication.Client.Validators;
+public class MaintainerEditVmValidator : AbstractValidator<MaintainerEditVm>
 {
-    public class MaintainerEditVmValidator : AbstractValidator<MaintainerEditVm>
+    public MaintainerEditVmValidator()
     {
-        public MaintainerEditVmValidator()
-        {
-            RuleFor(m => m.Name)
-                .NotEmpty()
-                    .WithMessage("The Maintainer Name is Required")
-                .MaximumLength(25)
-                    .WithMessage("The name of the Maintainer must be 25 characters or less")
-                .MinimumLength(3)
-                    .WithMessage("The name of the Maintainer must be at least 3 characters long");
-        }
+        RuleFor(m => m.Name)
+            .NotEmpty()
+                .WithMessage("The Maintainer Name is Required")
+            .MaximumLength(25)
+                .WithMessage("The name of the Maintainer must be 25 characters or less")
+            .MinimumLength(3)
+                .WithMessage("The name of the Maintainer must be at least 3 characters long");
     }
 }
