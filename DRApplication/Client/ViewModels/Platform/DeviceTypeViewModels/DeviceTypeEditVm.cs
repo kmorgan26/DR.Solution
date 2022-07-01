d7fb17e7f596eb2f5bea92ba5b3e3531ec5a7502
Kevin Morgan <kmorgan26@gmail.com>
2022-06-30 20:29:19 -0500

    Change Property Name

--- DRApplication/Client/ViewModels/Platform/DeviceTypeViewModels/DeviceTypeEditVm.cs
+++ DRApplication/Client/ViewModels/Platform/DeviceTypeViewModels/DeviceTypeEditVm.cs
@@ -2,8 +2,8 @@
 
 public class DeviceTypeEditVm
 {
-    public int Id { get; set; }
-    public string Name { get; set; } = null!;
-    public int MaintainerId { get; set; }
+    public int Id { get; set; } = 0;
+    public string Platform { get; set; } = string.Empty;
+    public int MaintainerId { get; set; } = 0;
     public bool IsActive { get; set; }
 }
