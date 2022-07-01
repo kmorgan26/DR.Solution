64e34eea8c631da8c500350c2e87ecf78d3a59ef
Kevin Morgan <kmorgan26@gmail.com>
2022-07-01 08:36:28 -0500

    Removed Data Annotations

--- DRApplication/Client/ViewModels/Platform/DeviceTypeViewModels/DeviceTypeInsertVm.cs
+++ DRApplication/Client/ViewModels/Platform/DeviceTypeViewModels/DeviceTypeInsertVm.cs
@@ -1,16 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace DRApplication.Client.ViewModels;
+﻿namespace DRApplication.Client.ViewModels;
 
 public class DeviceTypeInsertVm
 {
-    [Required]
-    [Range(1, 50, ErrorMessage = "Platform Name cannot be more than 50 characters long")]
-    public string Name { get; set; } = string.Empty;
-
-    [Required]
-    [Range(1, 100, ErrorMessage = "Please Select a Maintainer for this Device")]
+    public string Platform { get; set; } = string.Empty;
     public int MaintainerId { get; set; }
-    
     public bool IsActive { get; set; } = true;
 }
