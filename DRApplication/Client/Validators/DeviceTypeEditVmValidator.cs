3cfceab52c81eea5ea286bb3ba3371b13f05c7a3
Kevin Morgan <kmorgan26@gmail.com>
2022-06-30 20:28:46 -0500

    Change Property Name

--- DRApplication/Client/Validators/DeviceTypeEditVmValidator.cs
+++ DRApplication/Client/Validators/DeviceTypeEditVmValidator.cs
@@ -1,24 +1,22 @@
 ﻿using DRApplication.Client.ViewModels;
 using FluentValidation;
 
-namespace DRApplication.Client.Validators
+namespace DRApplication.Client.Validators;
+public class DeviceTypeEditVmValidator : AbstractValidator<DeviceTypeEditVm>
 {
-    public class DeviceTypeEditVmValidator : AbstractValidator<DeviceTypeEditVm>
+    public DeviceTypeEditVmValidator()
     {
-        public DeviceTypeEditVmValidator()
-        {
-            RuleFor(x => x.Name)
-                .NotEmpty()
-                    .WithMessage("The Platform Name is required. How else would we identify them?")
-                .MinimumLength(20)
-                    .WithMessage("The Platform Name cannot be more than 20 characters in length")
-                .MinimumLength(5)
-                    .WithMessage("The Platform Name cannot be less than 5 characters in length");
+        RuleFor(x => x.Platform)
+            .NotEmpty()
+                .WithMessage("The Platform Name is required. How else would we identify them?")
+            .MaximumLength(20)
+                .WithMessage("The Platform Name cannot be more than 20 characters in length")
+            .MinimumLength(5)
+                .WithMessage("The Platform Name cannot be less than 5 characters in length");
 
-            RuleFor(x => x.MaintainerId)
-                .NotNull()
-                .GreaterThan(0)
-                    .WithMessage("You must select a Maintainer");
-        }
+        RuleFor(x => x.MaintainerId)
+            .NotNull()
+            .GreaterThan(0)
+                .WithMessage("You must select a Maintainer");
     }
 }
