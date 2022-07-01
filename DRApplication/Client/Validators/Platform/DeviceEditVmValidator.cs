d52225837c59b204f4c336ce3cfe706ee2f8b4d2
Kevin Morgan <kmorgan26@gmail.com>
2022-07-01 08:46:18 -0500

    Format Namespace

--- DRApplication/Client/Validators/Platform/DeviceEditVmValidator.cs
+++ DRApplication/Client/Validators/Platform/DeviceEditVmValidator.cs
@@ -1,24 +1,22 @@
 ﻿using DRApplication.Client.ViewModels;
 using FluentValidation;
 
-namespace DRApplication.Client.Validators
+namespace DRApplication.Client.Validators;
+public class DeviceEditVmValidator: AbstractValidator<DeviceEditVm>
 {
-    public class DeviceEditVmValidator: AbstractValidator<DeviceEditVm>
+    public DeviceEditVmValidator()
     {
-        public DeviceEditVmValidator()
-        {
-            RuleFor(x => x.Device)
-            .NotEmpty()
-                .WithMessage("The Device Name is required. We gotta call them soemthing!")
-            .MaximumLength(20)
-                .WithMessage("The Device Name cannot be more than 20 characters in length")
-            .MinimumLength(4)
-                .WithMessage("The Device Name cannot be less than 4 characters in length");
+        RuleFor(x => x.Device)
+        .NotEmpty()
+            .WithMessage("The Device Name is required. We gotta call them soemthing!")
+        .MaximumLength(20)
+            .WithMessage("The Device Name cannot be more than 20 characters in length")
+        .MinimumLength(4)
+            .WithMessage("The Device Name cannot be less than 4 characters in length");
 
-            RuleFor(x => x.DeviceTypeId)
-                .NotNull()
-                .GreaterThan(0)
-                    .WithMessage("You must select a Platform");
-        }
+        RuleFor(x => x.DeviceTypeId)
+            .NotNull()
+            .GreaterThan(0)
+                .WithMessage("You must select a Platform");
     }
 }
