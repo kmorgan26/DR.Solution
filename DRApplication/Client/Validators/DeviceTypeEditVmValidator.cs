643c820e24f88a45acfe235fbac6e680ace76172
Kevin Morgan <kmorgan26@gmail.com>
2022-06-30 19:30:41 -0500

    Add DeviceTypeEditVm Validator

---
+++ DRApplication/Client/Validators/DeviceTypeEditVmValidator.cs
@@ -0,0 +1,24 @@
+﻿using DRApplication.Client.ViewModels;
+using FluentValidation;
+
+namespace DRApplication.Client.Validators
+{
+    public class DeviceTypeEditVmValidator : AbstractValidator<DeviceTypeEditVm>
+    {
+        public DeviceTypeEditVmValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                    .WithMessage("The Platform Name is required. How else would we identify them?")
+                .MinimumLength(20)
+                    .WithMessage("The Platform Name cannot be more than 20 characters in length")
+                .MinimumLength(5)
+                    .WithMessage("The Platform Name cannot be less than 5 characters in length");
+
+            RuleFor(x => x.MaintainerId)
+                .NotNull()
+                .GreaterThan(0)
+                    .WithMessage("You must select a Maintainer");
+        }
+    }
+}
