034941d0b4f45e50fcb7e9b2e25cdcdd91bcb3d8
Kevin D. Morgan <kevin.d.morgan@gdit.com>
2022-07-06 13:34:18 -0500

    Arrange validators in folders

--- DRApplication/Client/ViewModels/ConfigurationViewModels/HardwareConfigs/HardwareConfigInsertVm.cs
+++ DRApplication/Client/ViewModels/ConfigurationViewModels/HardwareConfigs/HardwareConfigInsertVm.cs
@@ -1,13 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
-namespace DRApplication.Client.ViewModels;
+﻿namespace DRApplication.Client.ViewModels;
+
 public class HardwareConfigInsertVm
 {
-    [Required]
-    [Range(1, 50, ErrorMessage = "Hardware Config Name cannot be more than 50 characters long")]
     public string Name { get; set; } = string.Empty;
-
-    [Required]
-    [Range(1, 100, ErrorMessage = "Please Select a Device Type")]
     public int DeviceTypeId { get; set; }
-
 }
