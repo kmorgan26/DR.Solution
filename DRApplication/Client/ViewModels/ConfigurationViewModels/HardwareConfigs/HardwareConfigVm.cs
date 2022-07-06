7e2adbaf88f7b7445c56e4566a5a4cd4809cc67c
Kevin D. Morgan <kevin.d.morgan@gdit.com>
2022-07-06 14:07:03 -0500

    remove unused using statement

--- DRApplication/Client/ViewModels/ConfigurationViewModels/HardwareConfigs/HardwareConfigVm.cs
+++ DRApplication/Client/ViewModels/ConfigurationViewModels/HardwareConfigs/HardwareConfigVm.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-namespace DRApplication.Client.ViewModels;
+﻿namespace DRApplication.Client.ViewModels;
 public class HardwareConfigVm
 {
     public int Id { get; set; } = 0;
