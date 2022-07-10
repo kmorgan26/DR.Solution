db0ceb715eefa0343485df51fb03b6e761030620
Kevin Morgan <kmorgan26@gmail.com>
2022-07-10 11:33:22 -0500

    Add Repo for Dapper AdHoc Queries

---
+++ DRApplication/Client/Services/AdHocRepository.cs
@@ -0,0 +1,50 @@
+﻿using DRApplication.Shared.Filters;
+using DRApplication.Shared.Interfaces;
+using DRApplication.Shared.Requests;
+using DRApplication.Shared.Responses;
+using Newtonsoft.Json;
+using System.Net.Http.Json;
+
+namespace DRApplication.Client.Services
+{
+    public class AdHocRepository<TEntity> : IAdHocRepository<TEntity> where TEntity : class
+    {
+        private readonly HttpClient _http;
+
+        public AdHocRepository(HttpClient http)
+        {
+            _http = http;
+        }
+        public async Task<IEnumerable<TEntity>> Get(AdhocRequest adhocRequest)
+        {
+            try
+            {
+                string url = adhocRequest.Url;
+                var result = await _http.PostAsJsonAsync(url, adhocRequest);
+                result.EnsureSuccessStatusCode();
+                string responseBody = await result.Content.ReadAsStringAsync();
+                var response = JsonConvert.DeserializeObject<APIListOfEntityResponse<TEntity>>(responseBody);
+
+                if (response is not null && response.Success)
+                    return response.Data;
+                else
+                    return new List<TEntity>();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        public Task<PagedResponse<TEntity>> GetAsync(QueryFilter<TEntity> Filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetByIdAsync(object Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DRApplication/Shared/Interfaces/IAdHocRepository.cs
@@ -0,0 +1,18 @@
+﻿using DRApplication.Shared.Filters;
+using DRApplication.Shared.Requests;
+using DRApplication.Shared.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRApplication.Shared.Interfaces;
+
+public interface IAdHocRepository<TEntity> where TEntity : class
+{
+    Task<PagedResponse<TEntity>> GetAsync(QueryFilter<TEntity> Filter);
+    Task<IEnumerable<TEntity>> Get(AdhocRequest adhocRequest);
+    Task<TEntity> GetByIdAsync(object Id);
+
+}
