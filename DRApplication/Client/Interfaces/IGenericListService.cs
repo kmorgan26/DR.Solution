7acd49d5cc6f079b54bcb5d759f03b26225f989c
Kevin Morgan <kmorgan26@gmail.com>
2022-07-15 09:38:17 -0500

    remove unused files and folders

--- DRApplication/Client/Interfaces/IGenericListService.cs
+++
@@ -1,9 +0,0 @@
-﻿using DRApplication.Shared.Enums;
-using DRApplication.Client.ViewModels;
-
-namespace DRApplication.Client.Interfaces;
-
-public interface IGenericListService
-{
-    Task<IEnumerable<GenericListVm>> GetGenericListVmsFromPlatformListName(PlatformListName listType);
-}
