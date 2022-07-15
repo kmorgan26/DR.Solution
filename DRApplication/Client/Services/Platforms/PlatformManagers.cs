db6e3e47342af3503c53eb92ef08ee518e4de173
Kevin Morgan <kmorgan26@gmail.com>
2022-07-15 09:43:51 -0500

    Consolidate Platform related managers to PlatformManagers.cs

---
+++ DRApplication/Client/Services/Platforms/PlatformManagers.cs
@@ -0,0 +1,34 @@
+﻿using DRApplication.Shared.Models;
+namespace DRApplication.Client.Services.Platforms
+{
+    public class DeviceManager : ApiRepository<Device>
+    {
+        private readonly HttpClient _http;
+
+        public DeviceManager(HttpClient http) : base(http, "device", "Id")
+        {
+            _http = http;
+        }
+    }
+    public class DeviceTypeManager : ApiRepository<DeviceType>
+    {
+        private readonly HttpClient _http;
+        static string controllerName = "devicetype";
+
+        public DeviceTypeManager(HttpClient http) : base(http, controllerName, "Id")
+        {
+            _http = http;
+        }
+    }
+    public class MaintainerManager : ApiRepository<Maintainer>
+    {
+        HttpClient _http;
+        static string controllerName = "maintainer";
+
+        public MaintainerManager(HttpClient http) : base(http, controllerName, "Id")
+        {
+            _http = http;
+        }
+    }
+
+}
