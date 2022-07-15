f2ddc1b0c967883a3be35585d5df9f31d334d2a7
Kevin Morgan <kmorgan26@gmail.com>
2022-07-15 09:41:38 -0500

    Consolidate Issue related managers into IssueManagers.cs

---
+++ DRApplication/Client/Services/Issues/IssueManagers.cs
@@ -0,0 +1,49 @@
+﻿using DRApplication.Shared.Models;
+namespace DRApplication.Client.Services.Issues
+{
+    public class CorrectiveActionManager : ApiRepository<CorrectiveAction>
+    {
+        private readonly HttpClient _http;
+
+        public CorrectiveActionManager(HttpClient http) : base(http, "correctiveaction", "Id")
+        {
+            _http = http;
+        }
+    }
+    public class DeviceDiscoveredManager : ApiRepository<DeviceDiscovered>
+    {
+        private readonly HttpClient _http;
+
+        public DeviceDiscoveredManager(HttpClient http) : base(http, "devicediscovered", "Id")
+        {
+            _http = http;
+        }
+    }
+    public class DrManager : ApiRepository<Dr>
+    {
+        private readonly HttpClient _http;
+
+        public DrManager(HttpClient http) : base(http, "dr", "Id")
+        {
+            _http = http;
+        }
+    }
+    public class IssueManager : ApiRepository<Issue>
+    {
+        private readonly HttpClient _http;
+
+        public IssueManager(HttpClient http) : base(http, "issue", "Id")
+        {
+            _http = http;
+        }
+    }
+    public class MaintIssueManager : ApiRepository<MaintIssue>
+    {
+        private readonly HttpClient _http;
+
+        public MaintIssueManager(HttpClient http) : base(http, "maintissue", "Id")
+        {
+            _http = http;
+        }
+    }
+}
